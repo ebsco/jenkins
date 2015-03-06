2c7d9fb7dc08acd3c70abe16ccf4178c93c9eadc
Brian McAllister <bmcallister@ebsco.com>
2015-03-06 13:48:43 -0500

    Changed version number for proper upstream dependency resolution

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'releng@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.1'
-
+#version          '2.2.1'
+version          '2015.03.06'
 recipe 'master', 'Installs a Jenkins master'
 
 depends 'apt',   '~> 2.0'
