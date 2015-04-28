3ad0c0efddca013f3aaf1971a8ca19a578360293
Brian McAllister <bmcallister@ebsco.com>
2015-04-28 12:40:21 -0400

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #version          '2.2.1'
-version          '2015.03.06'
+version          '2016.01.01'
 recipe 'master', 'Installs a Jenkins master'
 
 depends 'apt',   '~> 2.0'
